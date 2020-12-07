89d1c4b2877e9f4457fc18ccbd00079c9ed9c7e8
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-07 17:51:00 +0100

    Update Liste.c
    Changes squelette

--- Liste.c
+++ Liste.c
@@ -5,12 +5,6 @@
 //Fonction qui crée un noeud (élément)
 Node* create_elem(char car,int occ)
 {
-    Node* new_node=(Node*)malloc(sizeof(Node));
-    new_node->caractere = (char)car;
-    new_node->occurrence=occ;
-    new_node->next_elem=NULL;
-    new_node->left=NULL;
-    new_node->right=NULL;
     return new_node;
 }
 
@@ -18,14 +12,7 @@ Node* create_elem(char car,int occ)
 //Retourne l'addresse du noeud s'il est present
 //Retourne NULL si le noeud n'est pas present
 Node* increment_elem(Node* list_huffman,char caractere){
-    //printf("Entrée dans increment_elem\n");
-    while(list_huffman != NULL){
-        if ((list_huffman->caractere)==caractere){
-            list_huffman->occurrence++;
-            return list_huffman;
-        }
-        list_huffman=list_huffman->next_elem;
-    }
+    
     return NULL;
 }
 
@@ -33,54 +20,16 @@ Node* increment_elem(Node* list_huffman,char caractere){
 //Si le caractere courant n'est pas dans la liste : creation d'un noeud contenant le caractere et ajout de ce noeud dans la liste
 //Si le caractere courant est dans la liste : incrementation de son occurrence
 Node* create_list_char(char* file_in){
-    Node* mylist=NULL;
-    Node* temp=NULL;//liste temporaire
-    char car;
-    FILE* file=fopen(file_in,"r");
-
-    //printf("Entrée dans create_list_huffman\n");
-    if (file==NULL){ //problème d'ouverture du fichier
-        fprintf(stderr, "-------------------ERREUR D'OUVERTURE-------------------");
-    }
-
-    while((car=fgetc(file)) != EOF){//Tant que nous ne sommes pas arrivés à la fin du fichier texte
-        if (mylist==NULL){//Si la liste est vide
-            mylist=create_elem(fgetc(file),1);
-            temp=mylist;
-        }
-        else{
-            if (increment_elem(mylist,car) == NULL){
-                temp->next_elem=create_elem(car,1);
-                temp=temp->next_elem;
-            }
-        }
-    }
-    fclose(file);
+    
     return mylist;
 }
 
 void print_list(Node* mylist)
 {
-    if (mylist == NULL)
-    {
-        return;
-    }
-    else
-    {
-
-        printf("[%c][%d] -> ", mylist->caractere, mylist->repetition);
-        print_list(mylist->next);
-    }
+    return;
 }
 
 int nb_element_list(Node* mylist)
 {
-    if (mylist == NULL)
-    {
-        return 0;
-    }
-    else
-    {
-        return 1 + nb_element_list(mylist->next);
-    }
+    return 0;
 }
