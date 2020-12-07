d9348e040a8f9609d9536eb81b333e34602b4ced
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-07 17:55:45 +0100

    Update main.c
    changes after professors' explanation

--- main.c
+++ main.c
@@ -4,31 +4,6 @@
 
 int main()
 {
-
-   translate_binary();
-
-    FILE* fichier = fopen("Alice.txt","r");
-    FILE* fichier2 = fopen("Output.txt","r");
-
-    int x = nb_caractere(fichier);
-    int y = nb_caractere(fichier2);
-
-    printf("Le nombre de caractère dans Alice.txt est %d\n",x);
-    printf("Le nombre de caractère dans Output.txt est %d\n",y);
-fclose(fichier);
-fclose(fichier2);
-
-    Element* mylist=NULL;
-
-    mylist = Occurence();
-
-     print_list(mylist);
-    printf("\n\nIl y a %d lettres differentes \n",nb_element_list(mylist));
-
-Node2* HUFFMAN= NULL;
-HUFFMAN = tree_huffman(mylist);
-print_tree(HUFFMAN);
-
-Storage_Dictionary(HUFFMAN);
+   printf(" Welcome to the Project Huffman ! \n");
     return 0;
 }
