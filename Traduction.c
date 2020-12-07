38344ff4aeb8f82fd1ea24ca97daf69b4eaa2953
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-07 17:52:56 +0100

    Update Traduction.c
    changes squelette

--- Traduction.c
+++ Traduction.c
@@ -5,94 +5,23 @@
 
 int nb_caractere(FILE* fichier)
 {
-    int x = 0;
-    int Lettre;
-    if (fichier != NULL) // test opening file
-    {
-        Lettre = fgetc(fichier);//Reading first letter
-        while (Lettre != EOF)
-        {
-            x++;// repetition counter
-            Lettre = fgetc(fichier);//Letter next
-        }
-        fclose(fichier);//closing file
-        return x; // return number of repetition
-
-    }
-    else
-    {
-        // display error message
-        printf("Impossible d'ouvrir le fichier test.txt");
-        return 0;
-    }
+    return 0;
 }
 
 int ascii_to_binary(char car)
 {
-    int c = car;
-    if (c == 0)
-        return 0;
-    if (c == 1)
-        return 1;
-    return ((c % 2) + 10 * ascii_to_binary(c / 2));
+    return 0;
 }
 
 //Fonction permettant de traduire un fichier texte en code binaire (8 bits par caractere)
 /*Parametre : un fichier input.txt contenant le texte à traduire et un fichier output.txt
  dans lequel la traduction de input.txt en code binaire sera écrit*/
 void translate_binary(char* file_in, char* file_out){
-    int car;
-    FILE* input=fopen(file_in,"r");//on ouvre input.txt en mode lecture
-    FILE* output=fopen(file_out,"w");//on ouvre input.txt en mode écriture
-
-    if (input==NULL){ //problème d'ouverture du fichier
-        fprintf(stderr, "-------------------ERREUR D'OUVERTURE-------------------");
-    }
-
-    if(output==NULL)//problème d'ouverture du fichier
-    {
-        fprintf(stderr, "-------------------ERREUR D'OUVERTURE-------------------");
-    }
-
-    while((car=fgetc(input)) != EOF){//Tant que nous ne sommes pas arrivés à la fin du fichier texte
-        fprintf(output,"%08d",ascii_to_binary(car));//on écrit dans le fichier output.txt le code binaire du caractere courant.
-    }
-    fclose(input);//on ferme le fichier input.txt
-    fclose(output);//on ferme le fichier output.txt
+    
+    return;
 }
 
 void Encodage(FILE* Alice)
 {
-    FILE* Output2 = fopen("Output2.txt","w"); // Opening of the file Output 
-    FILE* Dico = fopen("Dico.txt", "r"); // Opening of the file Dico which is the dictionnary containing all the binary numbers correspondind to the caracters
-
-    caracter = fgetc(Alice);
-    caracterD = fgetc(Dico);
-
-    // Until that the the caracter is not at the end of the file do these
-    while( caracter != EOF)
-    {
-        while (caracterD != EOF) 
-        {
-            if(caracter == caracter D) // if the caracter in the file Alice corresponds to the caracter present in the dictionnary
-            {
-                while (caracterD != '\n') // until we go to the next line to see the next caracter
-                {
-                    for(i=0; i<4; i++)
-                    {
-                        fgetc(Dico); 
-                        printf("%d",caracterD);
-                    }
-                    
-                }
-            }
-                
-            else
-            {
-                fgetc("\n",Dico);
-            }
- 
-       }
-   
-    }
+    return;
 }
