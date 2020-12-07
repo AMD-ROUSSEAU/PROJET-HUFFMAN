e673c697d31f386e00a475af1f761a0e3d5a084c
AMD-ROUSSEAU <73126089+AMD-ROUSSEAU@users.noreply.github.com>
2020-12-07 18:12:41 +0100

    Update Compression.h
    Réorganisation du module

--- Compression.h
+++ Compression.h
@@ -1,36 +1,7 @@
 #ifndef FONCTION_H_INCLUDED
 #define FONCTION_H_INCLUDED
 
-typedef struct Element
-{
-    char caractere;
-    int repetition;
-    struct Element* next;
-}Element;
 
-typedef struct Node
-{
-    Element* data;
-    struct Node* left;
-    struct Node* right;
-}Node;
-
-typedef struct Node2{
-
-    char caractere;
-
-    int occurrence;
-
-    struct Node2* left;
-
-    struct Node2* right;
-
-}Node2;
-
-typedef struct List_Node
-{
-
-}List_Node;
 
 
 
