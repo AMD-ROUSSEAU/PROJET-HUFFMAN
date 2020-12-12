af5128f803583998b55379af2c3c06aa4c6e1fc1
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-12 11:47:52 +0100

    Create Dictionnaire.c

---
+++ Dictionnaire.c
@@ -0,0 +1,21 @@
+#include "Dictionnaire.h"
+
+/**
+ * \brief Function to write a caractere and its code in a text file
+ * \param dico the text file in which we write, mytree a Huffman tree, path the code of the caractere.
+ * \ return nothing.
+ */
+void storage_letter(FILE* dico,Node* mytree,char* path){
+    return;
+}
+
+/**
+ * \brief Function to call for function storage_letter
+ * \param mytree a Huffman tree, Dico_name the text file in which we write.
+ * \ return nothing.
+ */
+void Storage_Dictionary(Node* mytree,char* Dico_name){
+
+   return;
+}
+
