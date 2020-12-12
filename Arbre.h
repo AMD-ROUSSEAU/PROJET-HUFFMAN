9d411d64a3c287a86ecf6878f0417121b2f9d720
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-12 11:38:17 +0100

    Update Arbre.h

--- Arbre.h
+++ Arbre.h
@@ -1,21 +1,31 @@
-#ifndef FONCTION_H_INCLUDED
-#define FONCTION_H_INCLUDED
 
-//Définition de la structure Node
+#ifndef ARBRE_H_INCLUDED
+#define ARBRE_H_INCLUDED
+
+#include "Liste.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+/**
+ * \typedef struct Node
+ * \brief a Node is a node containing a caractere and its occurrence.
+ */
+
 typedef struct Node{
-    char caractere;
-    int occurrence;
-    int code[];
-    struct Node* next_elem;//pointe sur l'élément suivant de la liste
-    struct Node* left;//pointe sur le sous-arbre gauche
-    struct Node* right;//pointe sur le sous-arbre droit
+    int caractere;/**< the ascii code of the caractere contained in Node*/
+    int occurrence;/**< the occurrence of the caractere*/
+    struct Node* next_elem;/**< the reference to the next element of the list*/
+    struct Node* left;/**< the reference to the left sub-tree of the tree*/
+    struct Node* right;/**< the reference to the right sub-tree of the tree*/
 }Node;
 
-void print_tree(Node* tree);
 Node* smaller_element(Node* mylist);
-Node* delete_smaller(Node* list, Node* smaller);;
-Node* create_elem(char car,int occ);
+Node* delete_smaller(Node* list, Node* smaller);
 Node* create_tree_huffman(Node* list_node);
-int nb_element_tree_right(Node* Tree);
+void print_tree(Node* tree);
 
-#endif // FONCTION_H_INCLUDED
+
+#endif // ARBRE_H_INCLUDED
