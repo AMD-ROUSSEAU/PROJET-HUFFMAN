2b700ab9e8f7becdac2ffdbcaf72872c0f1da992
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-12 11:40:42 +0100

    Update Compression.c

--- Compression.c
+++ Compression.c
@@ -1,10 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include "FONCTION.h"
+#include <string.h>
+#include "Compression.h"
 
-void Compression()
-{
-    return;
+/*Chargement du dictionnaire en memoire*/
+
+
+code_car* create_elem_dic(){
+    
+    return new_elem;
+}
+
+/**
+ * \brief Function to load a dictionary in memory (in a tab)
+ * \param dico the text file containing the dictionary.
+ * \ return the address of the tab containing the dictionary.
+ */
+code_car* read_dico(char* dico){
+   
+    return first_code;
+}
+
+/*=====================================================================================================*/
+
+/*Encodage*/
+
+/**
+ * \brief Function to find the code of a caractere in the dictionary
+ * \param dico_list the dictionary, car a caractere.
+ * \ return the code of the caractere if the caractere is found in the dictionary or NULL if it is not found.
+ */
+char* find_code(code_car* dico_list,char car){
+    
+    return NULL;
+}
+
+/**
+ * \brief Function to compress a text file in another text file
+ * \param file_text the text file that needs to be compressed, compressed the text file in which the compressed code will be written and memory_dico the dictionary.
+ * \ return nothing.
+ */
+void compression_file(char* file_text,char* compressed, code_car* memory_dico){
+   return;
 }
 
