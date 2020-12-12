1ef3ef161c0c9b6157d8a39e510929ca162ac9d4
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-12 11:51:58 +0100

    Create Decompression.h

---
+++ Decompression.h
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+#include "Arbre.h"
+
+void storage_letter(FILE* dico,Node* mytree,char* path);
+void Storage_Dictionary(Node* mytree,char* Dico_name);
+
+#endif // DICTIONNAIRE_H_INCLUDED
