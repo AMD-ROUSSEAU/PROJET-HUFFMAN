0ec291b0eba6dd14c7dfd4a13b8f7341da08d1eb
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-12 11:20:09 +0100

    Update Traduction.h

--- Traduction.h
+++ Traduction.h
@@ -1,10 +1,8 @@
-#ifndef FONCTION_H_INCLUDED
-#define FONCTION_H_INCLUDED
-
+#ifndef TRADUCTION_H_INCLUDED
+#define TRADUCTION_H_INCLUDED
 
 int ascii_to_binary(char car);
 void translate_binary(char* file_in, char* file_out);
 int nb_caractere(FILE* fichier);
-void Encodage(FILE* Alice);
 
-#endif // FONCTION_H_INCLUDED
+#endif // TRADUCTION_H_INCLUDED
