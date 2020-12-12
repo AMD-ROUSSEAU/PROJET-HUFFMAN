8ab292e74f44b4c8bf2e8bea6bd0d9ec0a3f33e8
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-12 11:51:21 +0100

    Create Decompression.c

---
+++ Decompression.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "Decompression.h"
+
+/**
+ * \brief Function to find the caractere correspondind to a binary code
+ * \param code_lettre a binary code and dico the dictionary.
+ * \ return the caractere found in the dictionary corresponding to the binary code or -1 if the caractere is not found.
+ */
+int find_ascii(char* code_lettre, code_car* dico)
+{
+   
+    return -1;
+}
+
+/**
+ * \brief Function to decompress a text file
+ * \param f_decomp the text file in which the decompressed code will be written, f_comp the text file containing the compressed code, dico the dictionary.
+ * \ return nothing.
+ */
+void decompress_file(char *f_decomp,char* f_comp,code_car* dico)
+{
+    return;
+}
