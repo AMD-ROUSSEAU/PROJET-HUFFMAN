fc382f719d8d1eb03fbf8e3446d0885bcc255ae8
Matusa01 <73669574+Matusa01@users.noreply.github.com>
2020-12-12 11:37:31 +0100

    Update Arbre.c

--- Arbre.c
+++ Arbre.c
@@ -5,35 +5,44 @@
 #include "Liste.h"
 
 
-//Fonction qui crée un noeud
-Node* create_elem(char car,int occ)
-{
-    
-    return new_node;
-}
-
+/**
+ * \brief Function to search for the smallest element in a Node* List
+ * \ param mylist a node list containing each caractere and its occurrence.
+ * \ return the address of the smallest element of mylist.
+ */
 Node* smaller_element(Node* mylist)
 {
   
     return smaller;
 }
 
+/**
+ * \brief Function to erase an element frome the list
+ * \param mylist a node list containing each caractere and its occurrence and smaller the element that will be erased.
+ * \ return mylist without smaller in it.
+ */
 //Fonction qui supprime l'élément smaller de la liste et qui retourne la nouvelle liste
 Node* delete_smaller(Node* list, Node* smaller){
     return mylist;
 }
 
+/**
+ * \brief Function to create a Huffman tree from a node list
+ * \param mylist mylist a node list containing each caractere and its occurrence.
+ * \ return mylist with the new node created in it.
+ */
 Node* create_tree_huffman(Node* list_node){
     
     return mylist;
 }
 
+/**
+ * \brief Function to print a tree
+ * \param mytree a Huffman tree containing each caractere and its occurrence.
+ * \ return nothing.
+ */
 void print_tree(Node* tree)
 {
     return;
 }
 
-int nb_element_tree_right(Node* Tree)
-{
-   return 0;
-}
